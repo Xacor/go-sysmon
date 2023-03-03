58f658b08067d94fdddc353f82715ac113271597
Konstantin Riabov <k.d.riabov@gmail.com>
2023-03-03 23:25:29 +0300

    wip: added one way stream and protobuf changes

--- client/client.go
+++ client/client.go
@@ -3,10 +3,11 @@ package main
 import (
 	"context"
 	"flag"
+	"io"
 	"log"
 	"time"
 
-	pb "github.com/Xacor/go-sysmon/sysmon"
+	pb "github.com/Xacor/go-sysmon/proto"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/credentials/insecure"
 	"google.golang.org/protobuf/types/known/durationpb"
@@ -16,15 +17,24 @@ var (
 	serverAddr = flag.String("addr", "localhost:5000", "The server address in the format of host:port")
 )
 
-func printStat(client pb.SysMonClient, req *pb.Request) {
-	log.Println("Gettting system load...")
-	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+func printSnaphots(client pb.SysMonClient, req *pb.Request) {
+	log.Println("Getting snapshot...")
+	ctx, cancel := context.WithTimeout(context.Background(), 60*time.Second)
 	defer cancel()
-	stat, err := client.GetStatistic(ctx, req)
+	stream, err := client.GetSnapshot(ctx, req)
 	if err != nil {
-		log.Fatalf("client.etStatistic failed: %v", err)
+		log.Fatalf("client.GetSnapshot failed: %v", err)
 	}
-	log.Println(stat)
+	for {
+		snapshot, err := stream.Recv()
+		if err == io.EOF {
+			break
+		}
+		if err != nil {
+			log.Fatalf("client.GetSnaphot failed: %v", err)
+		}
+		log.Printf("Received snapshot: %v", snapshot)
+	}
 
 }
 
@@ -38,5 +48,5 @@ func main() {
 	defer conn.Close()
 	client := pb.NewSysMonClient(conn)
 
-	printStat(client, &pb.Request{RefreshRate: durationpb.New(time.Second * 5), RefreshInterval: durationpb.New(time.Second * 10)})
+	printSnaphots(client, &pb.Request{RefreshRate: durationpb.New(time.Second * 3), RefreshInterval: durationpb.New(time.Second * 5)})
 }
--- server/server.go
+++ server/server.go
@@ -1,39 +1,48 @@
 package main
 
 import (
-	"context"
 	"flag"
 	"fmt"
 	"log"
 	"net"
 	"sync"
+	"time"
 
-	pb "github.com/Xacor/go-sysmon/sysmon"
+	pb "github.com/Xacor/go-sysmon/proto"
 	"google.golang.org/grpc"
+	"google.golang.org/protobuf/types/known/timestamppb"
 )
 
 var (
-	tls      = flag.Bool("tls", false, "Connection uses TLS if true, else plain TCP")
-	certFile = flag.String("cert_file", "", "The TLS cert file")
-	keyFile  = flag.String("key_file", "", "The TLS key file")
-	port     = flag.Int("port", 5000, "The server port")
+	// tls      = flag.Bool("tls", false, "Connection uses TLS if true, else plain TCP")
+	// certFile = flag.String("cert_file", "", "The TLS cert file")
+	// keyFile  = flag.String("key_file", "", "The TLS key file")
+	port = flag.Int("port", 5000, "The server port")
 )
 
 type sysMonServer struct {
 	pb.UnimplementedSysMonServer
-	mu   sync.Mutex
-	stat pb.Statistic
+
+	mu       sync.Mutex
+	snapshot []pb.Snapshot
 }
 
-func (s *sysMonServer) GetStatistic(ctx context.Context, req *pb.Request) (*pb.Statistic, error) {
-	log.Println("Received GetStatistic:", req.RefreshInterval, req.RefreshRate)
-	return &pb.Statistic{
-		LoadAverage: &pb.LoadAverage{
-			Load1:  1,
-			Load5:  5,
-			Load15: 15,
-		},
-	}, nil
+func (s *sysMonServer) GetSnapshot(req *pb.Request, stream pb.SysMon_GetSnapshotServer) error {
+	log.Println("GetSnapshot:", req.RefreshRate.AsDuration(), req.RefreshInterval.AsDuration())
+	ticker := time.NewTicker(req.RefreshRate.AsDuration())
+
+	for range ticker.C {
+		// Calculate snapshot and send it here
+		err := stream.Send(&pb.Snapshot{
+			LoadAverage: &pb.LoadAverage{Load1: 1.5, Load5: 0.9, Load15: 0.9},
+			TimeCreated: timestamppb.New(time.Now()),
+		})
+		if err != nil {
+			return err
+		}
+	}
+	return nil
+
 }
 
 func main() {
